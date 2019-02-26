cb7716622922ee5fda1c5c55bed6909e32198dd5
AlexaneLG <leguern.a@laposte.net>
2019-02-26 11:44:27 +0100

    Use multiple objects to display content

--- js/components/cursor-listener.js
+++ js/components/cursor-listener.js
@@ -3,16 +3,24 @@ AFRAME.registerComponent('cursor-listener', {
     this.el.addEventListener('click', function (evt) {
     	var debugSection = document.getElementById('debug_section');
     	document.getElementById('section').style.zIndex = '2';
-    	/*document.getElementById('appendinnersection').innerHTML = "";
+    	document.getElementById('appendinnersection').innerHTML = "";
     	
     	if (this.id === "computer") {
     		var innerSectionProjects = document.getElementById("innerSectionProjects");
-    		document.getElementById("appendinnersection").appendChild(innerSectionProjects); 
+    		var innerSectionProjectsCopy = innerSectionProjects.cloneNode(true);
+    		document.getElementById("appendinnersection").appendChild(innerSectionProjectsCopy); 
     	}
     	if (this.id === "watercolor") {
     		var innerSectionSkills = document.getElementById("innerSectionSkills");
-    		document.getElementById("section").appendChild(innerSectionSkills); 
-    	}*/
+    		var innerSectionSkillsCopy = innerSectionSkills.cloneNode(true);
+    		document.getElementById("appendinnersection").appendChild(innerSectionSkillsCopy); 
+    	}
+    	if (this.id === "smartphone") {
+    		var innerSectionContact = document.getElementById("innerSectionContact");
+    		var innerSectionContactCopy = innerSectionContact.cloneNode(true);
+    		document.getElementById("appendinnersection").appendChild(innerSectionContactCopy); 
+    	}
+    	
 
     });
     /*this.el.addEventListener('hover', function (evt) {
